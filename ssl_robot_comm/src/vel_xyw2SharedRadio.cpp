a790d4df6b669e9fc74d47eeef82c96d492975f0
Nicolai Ommer <nicolai.ommer@gmail.com>
2015-12-10 12:54:16 +0100

    initial commit

---
+++ ssl_robot_comm/src/vel_xyw2SharedRadio.cpp
@@ -0,0 +1,85 @@
+/**
+ * Translate ssl_robot_msg::vel_xyw to SSL shared radio protobuf command
+ */
+
+#include "ros/ros.h"
+#include "ssl_robot_msg/vel_xyw.h"
+
+#include <string>
+#include <arpa/inet.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "radio_protocol_command.pb.h"
+#include "radio_protocol_wrapper.pb.h"
+
+
+int socket_fd = 0;
+int robot_id = 0;
+sockaddr_in server_address;
+
+void ClearCommandProtbuf(RadioProtocolCommand* cmd_ptr) {
+  RadioProtocolCommand& cmd = *cmd_ptr;
+  cmd.set_robot_id(0);
+  cmd.set_velocity_x(0);
+  cmd.set_velocity_y(0);
+  cmd.set_velocity_r(0);
+}
+
+void FillCommandProtobuf(uint32_t robot_id,
+                         float velocity_x,
+                         float velocity_y,
+                         float velocity_r,
+                         RadioProtocolCommand* cmd_ptr) {
+  RadioProtocolCommand& cmd = *cmd_ptr;
+  cmd.set_robot_id(robot_id);
+  cmd.set_velocity_x(velocity_x);
+  cmd.set_velocity_y(velocity_y);
+  cmd.set_velocity_r(velocity_r);
+}
+
+void senderCallback(const ssl_robot_msg::vel_xyw::ConstPtr& vel)
+{
+  RadioProtocolWrapper wrapper;
+  RadioProtocolCommand command;
+  std::string buffer;
+
+  FillCommandProtobuf(robot_id, vel->x, vel->y, vel->w, &command);
+  *wrapper.add_command() = command;
+  wrapper.SerializeToString(&buffer);
+  sendto(socket_fd,
+         buffer.data(),
+         buffer.length(),
+         0,
+         reinterpret_cast<sockaddr*>(&server_address),
+         sizeof(server_address));
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "ssl_robot_setvel_listener_real");
+
+  ros::param::param<int>("robot_id", robot_id, 0);
+  std::string server_name;
+  ros::param::param<std::string>("server_name", server_name, "192.168.20.254");
+  int port_number = 10010;
+
+  socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  bzero(&server_address, sizeof(server_address));
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = inet_addr(server_name.c_str());
+  server_address.sin_port = htons(port_number);
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("/ssl_robot/set_vel_xyw", 1, senderCallback);
+
+  ros::spin();
+
+  return 0;
+}
---
+++ ssl_robot_control/src/vel_xyw2motorCtrl.cpp
@@ -0,0 +1,67 @@
+/**
+ * Translate ssl_robot_msg::vel_xyw to motor controller commands for 
+ * gazebo
+ */
+
+#include "ros/ros.h"
+#include "ssl_robot_msg/vel_xyw.h"
+#include "std_msgs/Float64.h"
+
+#include <string>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159265359
+
+ros::Publisher ctrl_pub[4];
+double theta[4];
+double bot_radius = 0.08;
+double wheel_radius = 0.025;
+
+static double degToRad(double b)
+{
+	return b * PI / 180.0;
+}
+
+void senderCallback(const ssl_robot_msg::vel_xyw::ConstPtr& vel)
+{
+  // fr, rr, rl, fl
+  for(int i=0;i<4;i++)
+  {
+    double w = (
+              -sin(theta[i]) * vel->x 
+             + cos(theta[i]) * vel->y 
+             + bot_radius * vel->w
+           ) * 1.0/wheel_radius;
+    std_msgs::Float64 msg;
+    msg.data = w;
+    ctrl_pub[i].publish(msg);
+    ros::spinOnce();
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "ssl_robot_setvel_listener_sim");
+  
+  double alpha = degToRad(60);
+  double beta = degToRad(45);
+  theta[0] = alpha;
+  theta[1] = PI-beta;
+  theta[2] = PI + beta;
+  theta[3] = 2*PI-alpha;  
+
+  ros::NodeHandle n;
+
+  ctrl_pub[0] = n.advertise<std_msgs::Float64>("/ssl_robot/wheel_fr_vel_controller/command", 1);
+  ctrl_pub[1] = n.advertise<std_msgs::Float64>("/ssl_robot/wheel_rr_vel_controller/command", 1);
+  ctrl_pub[2] = n.advertise<std_msgs::Float64>("/ssl_robot/wheel_rl_vel_controller/command", 1);
+  ctrl_pub[3] = n.advertise<std_msgs::Float64>("/ssl_robot/wheel_fl_vel_controller/command", 1);
+  
+  ros::Subscriber sub = n.subscribe("/ssl_robot/set_vel_xyw", 1, senderCallback);
+
+  ros::spin();
+
+  return 0;
+}
